0bc03bb3a95c5d7beafcc870b9d3d71e281e62ed
hackingguy <akashchhabra710@gmail.com>
2021-01-11 22:38:53 +0530

    The MoviesFlix

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 //Encoding
+app.use('/favicon.ico', express.static('./public/imgs/favicon.ico'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
