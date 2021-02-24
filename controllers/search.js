40aac67e566581b734ae0724c67de47d6b4446e2
hackingguy <akashchhabra710@gmail.com>
2021-02-24 16:07:26 +0530

    Search Page Issue Resolved

--- controllers/search.js
+++ controllers/search.js
@@ -2,7 +2,7 @@ const Movie = require('../models/movie')
 const sanitizer = require('../utils/sanitize')
 exports.searchController = (req,res)=>{
     Movie.search(req.query.q).then(data=>{
-        res.render('index',{
+        res.render('home',{
             movies:data,
             sanitizer:sanitizer,
             search:true,
--- public/js/movie.js
+++ public/js/movie.js
@@ -44,19 +44,6 @@ function hlsStream() {
   }
 }
 
-function sideCollapse() {
-  var search = document.querySelector(".fa-search");
-  var nav = document.querySelector(".sidebar");
-  var burger = document.querySelector(".burger");
-  search.addEventListener("click", () => {
-    document.querySelector(".search").classList.toggle("form-active");
-  });
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("sidebar-active");
-  });
-}
-
-
 function start() {
   sideCollapse();
   hlsStream();
