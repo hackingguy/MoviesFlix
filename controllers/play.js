b5feae23c74e81c1942b1ca526dc54fe265ae39d
hackingguy <akashchhabra710@gmail.com>
2021-03-11 15:16:15 +0530

    HLS Streaming Issue Resolved

--- controllers/play.js
+++ controllers/play.js
@@ -3,6 +3,41 @@ const {getDirectLink} = require('../utils/getDirectLink')
 const sanitizer = require('../utils/sanitize')
 const axios = require("axios")
 
+//Heart Logic (Change This if streaming don't works!)
+async function getURL(id,play){
+    let link = play["source"]["0"];
+    //Check If URL is Working
+    try{
+        await axios.head(link);
+
+        if(link.includes('m3u8'))
+            link=play["source"]["0"];
+        else
+            link = process.env.CORS_URL + play["source"]["0"];
+    }
+    catch(err){
+        //Else Check if backup URL Available
+        try{
+            await axios.head(play["source_bk"]["0"]);
+
+            if(link.includes('m3u8'))
+                link=play["source_bk"]["0"];
+            else
+                link = process.env.CORS_URL + play["source_bk"]["0"];
+        }
+        //Else Scrap URL From API
+        catch(err){
+            let d = await getDirectLink(id,play["movie_source"]);
+            if(link.includes('m3u8'))
+                link = d["source"]["0"];
+            else
+                link = process.env.CORS_URL +  d["source"]["0"];
+        }
+    }
+    return link;
+}
+
+
 exports.playController= async(req,res)=>{
     
     let details = await Movie.getMovieDetails(req.query.id);
@@ -11,33 +46,8 @@ exports.playController= async(req,res)=>{
     if(!play["source"])
         return res.redirect("/");
 
-    let link = play["source"]["0"];
-
-    //Check If URL is Working
-    try{
-        if(link.includes("vidnext.net/ajax.php")){
-            throw new Error("err");
-        }
-        let a = await axios.head(link);
-        console.log(a);
-        link = process.env.CORS_URL + play["source"]["0"]
-    }
-    catch(err){
-        //Else Check if backup URL Available
-        try{
-            if(link.includes("vidnext.net/ajax.php")){
-                throw new Error("err");
-            }
-            let a = await axios.head(play["source_bk"]["0"]);
-            console.log(a);
-            link = process.env.CORS_URL + play["source_bk"]["0"]
-        }
-        //Else Scrap URL From API
-        catch(err){
-            let d = await getDirectLink(id,play["movie_source"]);
-            link = process.env.CORS_URL +  d["source"]["0"];
-        }
-    }
+    let link = await getURL(id,play);
+    
     res.render("movie",{
         title: play["title"],
         sourceLink: link,
